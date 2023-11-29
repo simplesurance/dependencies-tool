f07d258f9e291f84c58e15d0783c1878890e0b62
Fabian Holler <fabian.holler@simplesurance.de>
2023-11-29 16:50:12 +0100

    cmd: add testcase for exporting and importing a dependency-tree
    add testcase that exports a dependency-tree to a JSON file and then
    reads it to create a deploy-order

---
+++ internal/cmd/deployorder_test.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"bytes"
+	"path/filepath"
+	"testing"
+)
+
+func TestExportImport(t *testing.T) {
+	tmpdir := t.TempDir()
+	destFile := filepath.Join(tmpdir, "tree.json")
+
+	exportcmd := newExportCmd()
+	exportcmd.root = filepath.Join("..", "deps", "testdata")
+	exportcmd.region = "eu"
+	exportcmd.env = "stg"
+	exportcmd.destFile = destFile
+
+	err := exportcmd.run(exportcmd.Command, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployOrderCmd := newDeployOrder()
+
+	err = deployOrderCmd.PreRunE(nil, []string{destFile})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stdoutBuf := bytes.Buffer{}
+	deployOrderCmd.Command.SetOut(&stdoutBuf)
+
+	err = deployOrderCmd.run(deployOrderCmd.Command, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	const expectedOut = `"stg-eu-service"
+"postgres"
+"consul"
+"a-service"
+`
+	outStr := stdoutBuf.String()
+	if outStr != expectedOut {
+		t.Errorf("got deployorder:\n---\n%s---\n\nexpected:\n---\n%s\n---", outStr, expectedOut)
+	}
+}
